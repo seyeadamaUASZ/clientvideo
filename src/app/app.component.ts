a7ac6f92a136facbfc4653cb0a28fe793c7a8e7e
seyeadama03 <seyeadama03@outlook.com>
2019-08-09 02:33:31 +0000

    client-video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { VideoserviceService } from './videoservice.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'videoangular';
+  data:any;
+
+  constructor(private videoS: VideoserviceService){}
+
+  ngOnInit(){
+    this.videoS.getFiles()
+     .subscribe(resp=>{
+       this.data = resp;
+       console.log(this.data);
+     },error=>{
+       console.log(error);
+     })
+  }
 }
